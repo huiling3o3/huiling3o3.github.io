b193313bacb9e085bc7efe375596780fe8fbd915
Ng Hui Ling <68372136+huiling3o3@users.noreply.github.com>
2020-08-08 20:19:55 +0800

    Updates of Index page
    Remove unnecessary sections, clean up mobile view paddings and alignment
    update profile picture and links

--- assets/js/main.js
+++ assets/js/main.js
@@ -32,88 +32,88 @@
   });
 
   // Header scroll class
-  // $(window).scroll(function () {
-  //   if ($(this).scrollTop() > 100) {
-  //     $('#header').addClass('header-scrolled');
-  //   } else {
-  //     $('#header').removeClass('header-scrolled');
-  //   }
-  // });
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 100) {
+      $('#header').addClass('header-scrolled');
+    } else {
+      $('#header').removeClass('header-scrolled');
+    }
+  });
 
-  // if ($(window).scrollTop() > 100) {
-  //   $('#header').addClass('header-scrolled');
-  // }
+  if ($(window).scrollTop() > 100) {
+    $('#header').addClass('header-scrolled');
+  }
 
-  // var scrolltoOffset = $('#header').outerHeight() - 21;
-  // if (window.matchMedia("(max-width: 991px)").matches) {
-  //   scrolltoOffset += 20;
-  // }
-  // $(document).on('click', '.main-nav a, .mobile-nav a, .scrollto', function (e) {
-  //   if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-  //     var target = $(this.hash);
-  //     if (target.length) {
-  //       e.preventDefault();
+  var scrolltoOffset = $('#header').outerHeight() - 21;
+  if (window.matchMedia("(max-width: 991px)").matches) {
+    scrolltoOffset += 20;
+  }
+  $(document).on('click', '.main-nav a, .mobile-nav a, .scrollto', function (e) {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      if (target.length) {
+        e.preventDefault();
 
-  //       var scrollto = target.offset().top - scrolltoOffset;
+        var scrollto = target.offset().top - scrolltoOffset;
 
-  //       if ($(this).attr("href") == '#header') {
-  //         scrollto = 0;
-  //       }
+        if ($(this).attr("href") == '#header') {
+          scrollto = 0;
+        }
 
-  //       $('html, body').animate({
-  //         scrollTop: scrollto
-  //       }, 1500, 'easeInOutExpo');
+        $('html, body').animate({
+          scrollTop: scrollto
+        }, 1500, 'easeInOutExpo');
 
-  //       if ($(this).parents('.main-nav, .mobile-nav').length) {
-  //         $('.main-nav .active, .mobile-nav .active').removeClass('active');
-  //         $(this).closest('li').addClass('active');
-  //       }
+        if ($(this).parents('.main-nav, .mobile-nav').length) {
+          $('.main-nav .active, .mobile-nav .active').removeClass('active');
+          $(this).closest('li').addClass('active');
+        }
 
-  //       if ($('body').hasClass('mobile-nav-active')) {
-  //         $('body').removeClass('mobile-nav-active');
-  //         $('.mobile-nav-toggle i').toggleClass('fa-times fa-bars');
-  //         $('.mobile-nav-overly').fadeOut();
-  //       }
-  //       return false;
-  //     }
-  //   }
-  // });
+        if ($('body').hasClass('mobile-nav-active')) {
+          $('body').removeClass('mobile-nav-active');
+          $('.mobile-nav-toggle i').toggleClass('fa-times fa-bars');
+          $('.mobile-nav-overly').fadeOut();
+        }
+        return false;
+      }
+    }
+  });
 
-  // // Activate smooth scroll on page load with hash links in the url
-  // $(document).ready(function () {
-  //   if (window.location.hash) {
-  //     var initial_nav = window.location.hash;
-  //     if ($(initial_nav).length) {
-  //       var scrollto = $(initial_nav).offset().top - scrolltoOffset;
-  //       $('html, body').animate({
-  //         scrollTop: scrollto
-  //       }, 1500, 'easeInOutExpo');
-  //     }
-  //   }
-  // });
+  // Activate smooth scroll on page load with hash links in the url
+  $(document).ready(function () {
+    if (window.location.hash) {
+      var initial_nav = window.location.hash;
+      if ($(initial_nav).length) {
+        var scrollto = $(initial_nav).offset().top - scrolltoOffset;
+        $('html, body').animate({
+          scrollTop: scrollto
+        }, 1500, 'easeInOutExpo');
+      }
+    }
+  });
 
-  // // Navigation active state on scroll
-  // var nav_sections = $('section');
-  // var main_nav = $('.main-nav, .mobile-nav');
+  // Navigation active state on scroll
+  var nav_sections = $('section');
+  var main_nav = $('.main-nav, .mobile-nav');
 
-  // $(window).on('scroll', function () {
-  //   var cur_pos = $(this).scrollTop() + 200;
+  $(window).on('scroll', function () {
+    var cur_pos = $(this).scrollTop() + 200;
 
-  //   nav_sections.each(function () {
-  //     var top = $(this).offset().top,
-  //       bottom = top + $(this).outerHeight();
+    nav_sections.each(function () {
+      var top = $(this).offset().top,
+        bottom = top + $(this).outerHeight();
 
-  //     if (cur_pos >= top && cur_pos <= bottom) {
-  //       if (cur_pos <= bottom) {
-  //         main_nav.find('li').removeClass('active');
-  //       }
-  //       main_nav.find('a[href="#' + $(this).attr('id') + '"]').parent('li').addClass('active');
-  //     }
-  //     if (cur_pos < 300) {
-  //       $(".nav-menu ul:first li:first").addClass('active');
-  //     }
-  //   });
-  // });
+      if (cur_pos >= top && cur_pos <= bottom) {
+        if (cur_pos <= bottom) {
+          main_nav.find('li').removeClass('active');
+        }
+        main_nav.find('a[href="#' + $(this).attr('id') + '"]').parent('li').addClass('active');
+      }
+      if (cur_pos < 300) {
+        $(".nav-menu ul:first li:first").addClass('active');
+      }
+    });
+  });
 
   // Mobile Navigation
   if ($('.main-nav').length) {
@@ -150,19 +150,6 @@
     $(".mobile-nav, .mobile-nav-toggle").hide();
   }
 
-  // jQuery counterUp (used in Whu Us section)
-  $('[data-toggle="counter-up"]').counterUp({
-    delay: 10,
-    time: 1000
-  });
-
-  // Testimonials carousel (uses the Owl Carousel library)
-  $(".testimonials-carousel").owlCarousel({
-    autoplay: true,
-    dots: true,
-    loop: true,
-    items: 1
-  });
 
   // Init AOS
   function aos_init() {
