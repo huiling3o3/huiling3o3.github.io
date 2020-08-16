94d0d3ee8c7b479c110849fd171d367b68c0c79c
davier <davier570@gmail.com>
2020-08-16 18:25:57 +0800

    add in sync button and popup

---
+++ assets/js/popup.js
@@ -0,0 +1,20 @@
+
+var modal = document.getElementById("popup-modal");
+
+var btn = document.getElementById("open-popup");
+
+var span = document.getElementById("close");
+
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
