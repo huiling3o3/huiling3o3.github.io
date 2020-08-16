36bb52e0081bc86a0602af1a2d5560aacad8bf36
Ng Hui Ling <68372136+huiling3o3@users.noreply.github.com>
2020-08-16 15:19:33 +0800

    Fixed the the active state on nav
    remove the nav active state on scroll function

--- assets/js/main.js
+++ assets/js/main.js
@@ -48,6 +48,7 @@
   if (window.matchMedia("(max-width: 991px)").matches) {
     scrolltoOffset += 20;
   }
+
   $(document).on('click', '.main-nav a, .mobile-nav a, .scrollto', function (e) {
     if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
       var target = $(this.hash);
@@ -90,29 +91,6 @@
         }, 1500, 'easeInOutExpo');
       }
     }
-  });
-
-  // Navigation active state on scroll
-  var nav_sections = $('section');
-  var main_nav = $('.main-nav, .mobile-nav');
-
-  $(window).on('scroll', function () {
-    var cur_pos = $(this).scrollTop() + 200;
-
-    nav_sections.each(function () {
-      var top = $(this).offset().top,
-        bottom = top + $(this).outerHeight();
-
-      if (cur_pos >= top && cur_pos <= bottom) {
-        if (cur_pos <= bottom) {
-          main_nav.find('li').removeClass('active');
-        }
-        main_nav.find('a[href="#' + $(this).attr('id') + '"]').parent('li').addClass('active');
-      }
-      if (cur_pos < 300) {
-        $(".nav-menu ul:first li:first").addClass('active');
-      }
-    });
   });
 
   // Mobile Navigation
